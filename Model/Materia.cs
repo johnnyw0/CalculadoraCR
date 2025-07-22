e12bf0c45cf02805c7d3e2604fd93f073aa55d60
johnnyw0 <johnnyawatanabe@gmail.com>
2025-07-22 15:51:50 -0300

    Add project files.

---
+++ Model/Materia.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraCR
+{
+    public class Materia {
+
+        public string Nome { get; set; }
+        public double Media { get; set; }
+        public int CargaHoraria { get; set; }
+
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CalculadoraCR
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
